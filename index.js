763bacfe444fdc8a8a6d92d108927860455581b9
michele-bertolino-accenture <michele.bertolino@accenture.com>
2024-08-01 13:55:26 +0200

    build: create grunt tasks for automate file generation

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const { readAndProcessFiles } = require('./merge_profiles_DACH.js');
 
-const {createTestProfileFull, createTestProfileLite} = require('./__tests__/utility.js')
-const fs  = require('fs');
-const { Readable, PassThrough } = require('stream');
-
-
 (async () => {
     console.log("Starting the merging process");
     await readAndProcessFiles();
